e394ee3ee93f96d4db2f45b037184b6277a3f7ec
Ali Nmb <n.ali75012@gmail.com>
2023-06-29 18:36:50 +0200

    Feat : Ajout des images dynamiquement + filtres via boutons

---
+++ script.js
@@ -0,0 +1,112 @@
+const gallerySection = document.querySelector('.gallery');
+const btnAll = document.querySelector('.btnAll');
+const btnObj = document.querySelector('.btnObj');
+const btnApp = document.querySelector('.btnApp');
+const btnHotels = document.querySelector('.btnHotels');
+
+function createFigure(link) {
+    const figureElm = document.createElement('figure');
+    const imgElm = document.createElement('img');
+    const figCaptionElm = document.createElement('figcaption');
+    imgElm.setAttribute('src', link.imageUrl);
+    imgElm.setAttribute('alt', link.title);
+    figCaptionElm.innerHTML = link.title;
+    figureElm.appendChild(imgElm);
+    figureElm.appendChild(figCaptionElm);
+    gallerySection.appendChild(figureElm);
+}
+
+const getAll = () => {
+    fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .then((data) => {
+
+            for (project of data) {
+                createProject();
+                console.log(project)
+            }
+        })
+        .catch(error => console.log("FETCH ERROR"))
+}
+getAll()
+
+function createProject() {
+    createFigure(project)
+}
+// Btn AddEventListener
+btnAll.addEventListener('click', function () {
+    gallerySection.innerHTML = "";
+    getAll()
+})
+
+/***** OBJETS *****/
+const getObjects = () => {
+    fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .then((data) => {
+
+            for (project2 of data) {
+                createObjets();
+            }
+        })
+        .catch(error => console.log("FETCH ERROR"))
+}
+
+function createObjets() {
+    if (project2.categoryId === 1) {
+        createFigure(project2)
+    }
+}
+// Btn AddEventListener
+btnObj.addEventListener('click', function () {
+    gallerySection.innerHTML = "";
+    getObjects()
+})
+
+/***** APPARTEMENTS *****/
+const getApp = () => {
+    fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .then((data) => {
+
+            for (project3 of data) {
+                createApp();
+            }
+        })
+        .catch(error => console.log("FETCH ERROR"))
+}
+
+function createApp() {
+    if (project3.categoryId === 2) {
+        createFigure(project3)
+    }
+}
+// Btn AddEventListener
+btnApp.addEventListener('click', function () {
+    gallerySection.innerHTML = "";
+    getApp()
+})
+
+/***** HÔTELS *****/
+const getHotels = () => {
+    fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .then((data) => {
+
+            for (project4 of data) {
+                createHotels();
+            }
+        })
+        .catch(error => console.log("FETCH ERROR"))
+}
+
+function createHotels() {
+    if (project4.categoryId === 3) {
+        createFigure(project4)
+    }
+}
+// Btn AddEventListener
+btnHotels.addEventListener('click', function () {
+    gallerySection.innerHTML = "";
+    getHotels()
+})
