0d5397bc2b4b897d97bc24966ba90286ca458aa8
Ali Nmb <n.ali75012@gmail.com>
2023-07-02 14:25:51 +0200

    Fix: Retrait du surplus commenté de la première version

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,41 +2,38 @@ const submitBtn = document.getElementById('submitBtn');
 const invalide = document.querySelector('.invalide');
 
 async function login() {
-
+  
   const account = {
     email: (document.getElementById("email2").value),
     password: (document.getElementById("password2").value)
   }
 
-  if (!account.email || !account.password) {
-    invalide.innerText = "Erreur, mot de passe ou e-mail invalide !";
-    return;
-  } else {
+  const chargeUtile = JSON.stringify(account)
 
     fetch('http://localhost:5678/api/users/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(account)
+      body: chargeUtile
     })
       .then(response => {
-        if (response.ok) {
+        if (response.ok /*status 200*/) {
           return response.json();
           
         } else {
-          invalide.innerText = "Erreur requête."
+          invalide.innerText = "Erreur, mot de passe ou e-mail invalide !" /*status 401 unauthorized 404 user not found*/
         }
         /*console.log(response)*/
       })
       .then(response => {
         if (response) {
-          window.sessionStorage.setItem('infoUseur', JSON.stringify(response));
+          /*window.sessionStorage.setItem('infoUseur', JSON.stringify(response.userId));*/ // Uniquement le token ?
           window.sessionStorage.setItem('token', response.token);
           window.location.replace('./index.html');
           console.log(response)
         }
         
       })
-  }
+
 
 }
 
