3f14ed1c41530dcc675dd8c52b1b31bd81756815
Ali Nmb <n.ali75012@gmail.com>
2023-07-03 16:56:57 +0200

    Feat : Ajout addWorks() ; Non fonctionnel

--- FrontEnd/assets/panel.js
+++ FrontEnd/assets/panel.js
@@ -19,7 +19,7 @@ const getAll = () => {
     url.then((data) => {
         for (project of data) {
             createFigure(project)
-            //console.log(project)
+            /*console.log(project)*/
         }
     })
         .catch(error => console.log("FETCH ERROR"))
@@ -40,12 +40,14 @@ logoutBtn.addEventListener('click', (event) => {
 const modalContainer = document.querySelector('.modal-container')
 const openModal = document.querySelector('#openModal')
 const closeModal = document.querySelector('.close-modal')
+const closeModal2 = document.querySelector('.close-modal2')
 const galleryPhoto = document.querySelector('.gallery-photo')
 
 const modal1 = document.querySelector('.modal1');
 const addBtn = document.querySelector('.add-btn');
 
 const modal2 = document.querySelector('.modal2');
+const lastModal = document.querySelector('.last-modal')
 
 
 function createGalerie(link) {
@@ -63,12 +65,18 @@ function createGalerie(link) {
     figureElm.appendChild(trashElm);
     galleryPhoto.appendChild(figureElm);
 }
-
+closeModal.addEventListener('click', function () {
+    modalContainer.style.display = "none";
+})
+closeModal2.addEventListener('click', function () {
+    modalContainer.style.display = "none";
+})
 
 openModal.addEventListener('click', function () {
     galleryPhoto.innerHTML = "";
     modalContainer.style.display = "flex";
     modal1.style.display = "block";
+    modal2.style.display = "none";
 
     url.then((data) => {
         for (project of data) {
@@ -83,6 +91,63 @@ addBtn.addEventListener('click', function () {
     modal1.style.display = "none";
     modal2.style.display = "block";
 })
-closeModal.addEventListener('click', function () {
-    modalContainer.style.display = "none";
+
+lastModal.addEventListener('click', function () {
+    modal1.style.display = "block";
+    modal2.style.display = "none";
+})
+
+// Crée les catégories 
+
+const categorie = document.querySelector('#categorie');
+
+function createOptions (cat) {
+    const option = document.createElement('option');
+    option.setAttribute('value', cat.name);
+    option.innerHTML = cat.name;
+    categorie.appendChild(option);
+}
+
+function getOptions () {
+    fetch('http://localhost:5678/api/categories')
+    .then(response => response.json())
+    .then((data) => {
+        for (cate of data) {
+            createOptions(cate)
+            //console.log(project)
+        }
+    })
+        .catch(error => console.log("FETCH ERROR"))
+}
+
+getOptions();
+
+const btnValid = document.querySelector('.btn-valid');
+
+async function addWorks () {
+
+    const works = {
+        file: (document.getElementById("file").files[0]),
+        title: (document.getElementById("title").value),
+        categorie: (document.getElementById("categorie").value)
+      }
+
+      const chargeUtile = JSON.stringify(works)
+
+      fetch('http://localhost:5678/api/works', {
+        method : 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'multipart/form-data'
+        },
+        body: chargeUtile
+      })
+      .then(res => res.json())
+      .then(data => console.log(data)).catch(err => console.log(err))
+}
+
+btnValid.addEventListener('click', function (e) {
+    e.preventDefault();
+    addWorks()
+
 })
