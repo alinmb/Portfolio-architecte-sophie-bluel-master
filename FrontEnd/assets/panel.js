34ae93779e43822e08883ff94e94a9c29141fb77
Ali Nmb <n.ali75012@gmail.com>
2023-07-04 17:24:44 +0200

    Feat : Ajout d'image via upload dans modal et page d'accueil - Reader non intégré

--- FrontEnd/assets/panel.js
+++ FrontEnd/assets/panel.js
@@ -33,7 +33,7 @@ logoutBtn.addEventListener('click', (event) => {
     event.preventDefault();
     window.sessionStorage.removeItem('token');
     window.location.replace('./index.html');
-  })
+})
 
 // MODALES
 
@@ -49,13 +49,13 @@ const addBtn = document.querySelector('.add-btn');
 const modal2 = document.querySelector('.modal2');
 const lastModal = document.querySelector('.last-modal')
 
-
+// Creation des galerie des modales
 function createGalerie(link) {
     const figureElm = document.createElement('figure');
     const imgElm = document.createElement('img');
     const figCaptionElm = document.createElement('figcaption');
     const trashElm = document.createElement('i');
-    trashElm.setAttribute('class',"fa-solid fa-trash-can")
+    trashElm.setAttribute('class', "fa-solid fa-trash-can")
     imgElm.setAttribute('src', link.imageUrl);
     imgElm.setAttribute('alt', link.title);
     imgElm.classList.add('gallery-img')
@@ -72,12 +72,18 @@ closeModal2.addEventListener('click', function () {
     modalContainer.style.display = "none";
 })
 
+window.onclick = function (event) {
+    if (event.target == modalContainer) {
+        modalContainer.style.display = "none";
+    }
+}
+
 openModal.addEventListener('click', function () {
     galleryPhoto.innerHTML = "";
     modalContainer.style.display = "flex";
     modal1.style.display = "block";
     modal2.style.display = "none";
-
+    // Affichage de la gallerie dans la modale
     url.then((data) => {
         for (project of data) {
             createGalerie(project)
@@ -85,7 +91,7 @@ openModal.addEventListener('click', function () {
         }
     })
         .catch(error => console.log("FETCH ERROR"))
- 
+
 })
 addBtn.addEventListener('click', function () {
     modal1.style.display = "none";
@@ -101,53 +107,134 @@ lastModal.addEventListener('click', function () {
 
 const categorie = document.querySelector('#categorie');
 
-function createOptions (cat) {
+function createOptions(cat) {
     const option = document.createElement('option');
-    option.setAttribute('value', cat.name);
+    option.setAttribute('value', cat.id);
     option.innerHTML = cat.name;
     categorie.appendChild(option);
 }
 
-function getOptions () {
+function getOptions() {
     fetch('http://localhost:5678/api/categories')
-    .then(response => response.json())
-    .then((data) => {
-        for (cate of data) {
-            createOptions(cate)
-            //console.log(project)
-        }
-    })
-        .catch(error => console.log("FETCH ERROR"))
+        .then(response => response.json())
+        .then((data) => {
+            for (cate of data) {
+                createOptions(cate)
+                /*console.log(cate)*/
+            }
+        })
+        .catch(error => console.log("FETCH OPTIONS ERROR"))
 }
 
 getOptions();
 
 const btnValid = document.querySelector('.btn-valid');
 
-async function addWorks () {
 
-    const works = {
-        file: (document.getElementById("file").files[0]),
-        title: (document.getElementById("title").value),
-        categorie: (document.getElementById("categorie").value)
-      }
 
-      const chargeUtile = JSON.stringify(works)
+// async function addWorks () {
 
-      fetch('http://localhost:5678/api/works', {
-        method : 'POST',
+/*const works = {
+    image: (document.getElementById("file").files[0]),
+    title: (document.getElementById("title").value),
+    category: (document.getElementById("categorie").value)
+  }
+
+  const chargeUtile = JSON.stringify(works)*/
+//   const image = document.getElementById("file").files[0];
+//   const title = document.getElementById("title").value;
+//   const category = document.getElementById("categorie").value;
+
+//   const formData = new FormData();
+
+//   formData.append('image', image)
+//   formData.append('title', title)
+//   formData.append('category', category)
+
+//       fetch('http://localhost:5678/api/works', {
+//         method : 'POST',
+//         headers: {
+//             'accept': 'application/json',
+//             'Content-Type': 'multipart/form-data',
+//             'authorization': `Bearer ${userToken}`
+//         },
+//         body: formData
+//       })
+//       .then(res => {
+//         if (res.ok) {
+//             return res.json()
+//         } else {
+//             console.log('FETCH ERROR')
+//         }
+//       })
+//       .then(data => console.log(data))
+//       .catch(err => console.log(err))
+// }
+
+
+let userToken = window.sessionStorage.getItem("token");
+
+function filledInput() {
+    if (document.getElementById("title").value !== "" && document.getElementById("file").files[0] !== "") {
+       btnValid.style.background = "#1D6154";
+    }  else {
+        btnValid.style.background = "#A7A7A7";
+    }
+ }
+
+function addWorks() {
+
+    const image = document.getElementById("file").files[0];
+    const title = document.getElementById("title").value;
+    const category = document.getElementById("categorie").value;
+
+    const formData = new FormData();
+
+    formData.append('image', image)
+    formData.append('title', title)
+    formData.append('category', parseInt(category))
+
+    // console.log(image)
+    // console.log(formData)
+
+    fetch('http://localhost:5678/api/works', {
+        method: 'POST',
         headers: {
-            'accept': 'application/json',
-            'Content-Type': 'multipart/form-data'
+            'authorization': `Bearer ${userToken}`
         },
-        body: chargeUtile
-      })
-      .then(res => res.json())
-      .then(data => console.log(data)).catch(err => console.log(err))
+        body: formData
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+
+    const workAdded = document.createElement('figure');
+    const imgAdded = document.createElement('img');
+    const figcaption = document.createElement('figcaption');
+
+    imgAdded.setAttribute('alt', title);
+    figcaption.setAttribute('id', category);
+    figcaption.innerHTML = title;
+
+    gallerySection.appendChild(workAdded);
+    gallerySection.appendChild(imgAdded);
+
+    
 }
 
-btnValid.addEventListener('click', function (e) {
-    e.preventDefault();
-    addWorks()
-
+btnValid.addEventListener('click', function (event) {
+    event.preventDefault();
+    addWorks();
 })
+
+
+// function createWorks () {
+//     const workAdded = document.createElement('figure');
+//     const imgAdded = document.createElement('img');
+//     imgAdded.setAttribute('alt', title);
+//     const figcaption = document.createElement('figcaption');
+//     figcaption.setAttribute('id', category);
+//     figcaption.innerHTML = title;
+
+//     gallerySection.appendChild(workAdded);
+// }
